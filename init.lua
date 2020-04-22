03c073afa28574bb5dff3e71f30826d3bbcde632
Oskar <saturdaysheroes.business@gmail.com>
2020-04-22 09:06:47 +0200

    updated bot status

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ discordia.extensions()
 
 -- Config variables/tables.
 config.prefix = ">"
-config.token = ""
+config.token = "NzAyMjY5MzE0NzU3MDM0MDU1.Xp_r_Q.pdrToDCh_wL2_a2gv159NqNjw6o"
 config.activity = ">help"
 config.apiurl = "https://api.covid19api.com/"
 config.devmode = false
@@ -26,7 +26,7 @@ require("./modules/timer.lua")(client)
 require("./modules/commands.lua")(client)
 
 client:on("ready", function()
-    client:setGame(config.activity)
+    client:setGame(config.activity.." | "..client.guilds:count().." Serwerów")
     print("----------------------------------------------------------------------------")
 end)
 client:run("Bot "..config.token)
