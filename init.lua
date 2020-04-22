661ca06c0325d3f8015d630fc6ef6c8f6970e31e
Oskar <saturdaysheroes.business@gmail.com>
2020-04-22 15:36:48 +0200

    register languages on init

--- init.lua
+++ init.lua
@@ -8,25 +8,32 @@ local client = discordia.Client()
 _G.config = {}
 _G.util = {}
 _G.api = {}
+_G.lang = {}
+_G.languages = {}
 
 -- Additonal extensions. 
 discordia.extensions()
 
 -- Config variables/tables.
 config.prefix = ">"
-config.token = "NzAyMjY5MzE0NzU3MDM0MDU1.Xp_r_Q.pdrToDCh_wL2_a2gv159NqNjw6o"
+config.token = ""
 config.activity = ">help"
 config.apiurl = "https://api.covid19api.com/"
 config.devmode = false
+config.language = "english"
 
 -- Lua modules. 
 require("./modules/util.lua")(client)
 require("./modules/api.lua")(client)
-require("./modules/timer.lua")(client)
+require("./modules/language.lua")(client)
 require("./modules/commands.lua")(client)
 
 client:on("ready", function()
-    client:setGame(config.activity.." | "..client.guilds:count().." Serwerów")
+    client:setGame(config.activity.." | "..client.guilds:count().." Servers")
     print("----------------------------------------------------------------------------")
 end)
 client:run("Bot "..config.token)
+
+-- Languages.
+lang.RegisterLanguage("polish", "pl")
+lang.RegisterLanguage("english", "en")
